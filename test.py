3a2ad69251e8dd699d519e857ae08ee4eaf7b0b3
Trinh-Quoc-Trong <trinhtrong2423@gmail.com>
2024-12-07 08:10:46 +0700

    trong dep trai da push detec obj

--- test.py
+++ test.py
@@ -1,32 +1,12 @@
-from flask import Flask, request, render_template, send_from_directory, Response,send_file,url_for, jsonify
-import os
-from io import BytesIO
-import numpy as np
-import clip
-import torch
-from PIL import Image
-from tqdm import tqdm
-import requests
-import base64
-import re
-import json
-from word_processing import VietnameseTextProcessor
-from pathlib import Path
-FRAMES_DIR = "E:\\THIHE\\testfitty one\\SegmentVideo\\seg1\\SegmentVideo"
-FRAMES_JSON = "output_samples.json" 
-all = sorted([f.name for f in Path(FRAMES_DIR).iterdir()], key=lambda x: int(Path(x).stem))
-print(all)
+# fruits = (["apple", "banana", "cherry"], ["orange", "cac", "caonggiac"])
+# newlist = [x for y in fruits for x in y ]
 
-def load_frames_from_json(json_path):
-    """Load danh sách tên file từ file JSON."""
-    with open(json_path, 'r', encoding='utf-8') as file:
-        samples = json.load(file)
-    # Trả về danh sách tên file (không bao gồm đường dẫn đầy đủ)
-    return [os.path.basename(sample["filepath"]) for sample in samples if "filepath" in sample]
 
-a  = sorted(load_frames_from_json(FRAMES_JSON), key=lambda x: int(Path(x).stem))
-print("frame trong JSON")
-print(a)
-k =len(load_frames_from_json(FRAMES_JSON))
-print("so luong frame ")
-print(k)
+# l1 = (1, 2, 34, 4, 5, 2, 3, 412, 3, 13)  
+# my_list = [item - 1 for item in l1[2:5]]  # Bắt đầu từ chỉ số 2  
+
+# print(my_list)
+
+l1 = (1,2,34,4,5,2,3,412,3,13,)
+list = [item-1 for item in l1[1:]]
+print(list)
